b1c3fe543bcadb4df2f6bf644b8b9b28538bf193
Salted-Toast <104316350+morrison-page@users.noreply.github.com>
2023-04-08 22:35:44 +0100

    ...

--- passgen.py
+++ passgen.py
@@ -5,23 +5,38 @@ import argparse
 import random
 import string
 
-def generate_password(length):
-    charset = string.ascii_letters + string.digits + string.punctuation
+def generate_password(length, lowercase, uppercase, digits, punctuation):
+    charset = ''
+    if lowercase:
+        charset += string.ascii_lowercase
+    if uppercase:
+        charset += string.ascii_uppercase
+    if digits:
+        charset += string.digits
+    if punctuation:
+        charset += string.punctuation
+    if not charset:
+        raise ValueError('At least one character set must be selected. Use --lowercase, --uppercase, --digits, and/or --punctuation options.')
     password = ''.join(random.choice(charset) for _ in range(length))
     return password
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', '--length', type=int, default=12)
+    parser.add_argument('--lowercase', action='store_true')
+    parser.add_argument('--uppercase', action='store_true')
+    parser.add_argument('--digits', action='store_true')
+    parser.add_argument('--punctuation', action='store_true')
     args = parser.parse_args()
-    password = generate_password(args.length)
+
+    try:
+        password = generate_password(args.length, args.lowercase, args.uppercase, args.digits, args.punctuation)
+    except ValueError as e:
+        print(str(e))
+        return
+
     print(password)
 
 if __name__ == '__main__':
     sys.exit(main())
-
-# Fucntionality to add later #
-# number of passwords to generate
-# lowercase only
-# uppercase only
-# a man page
